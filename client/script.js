723bf8dbbe4df1eeba95eb11dfab198fd39a86d2
Andrew Mikofalvy <mikofalv@mit.edu>
2014-01-10 13:27:50 -0500

    added geolocation service to set initial gmap location

--- client/script.js
+++ client/script.js
@@ -15,7 +15,11 @@ if (Meteor.isClient) {
         $("#signUp").fadeIn()
       }, 500);
     });
-
+    
+    var initialLocation;
+    var mitCampus = new google.maps.LatLng (42.357,-71.09);
+    var browserSupportFlag = new Boolean();
+  
     $("#signUp").click(function() {
       $("#tint").fadeOut();
       $(this).fadeOut();
@@ -30,15 +34,39 @@ if (Meteor.isClient) {
 
   Template.body.rendered = function() {
     var mapOptions = {
-          center: new google.maps.LatLng(42.357, -71.09),
           zoom: 15,
           disableDefaultUI: true
-        };
-        var map = new google.maps.Map(document.getElementById("map-canvas"),
-            mapOptions);
-      }
+    };
+    var map = new google.maps.Map(document.getElementById("map-canvas"),
+        mapOptions);
 
-}
+    //Attempt W3C Geolocation
+    if (navigator.geolocation){
+      browserSupportFlag = true;
+      navigator.geolocation.getCurrentPosition(function(position) {
+        initialLocation = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+        map.setCenter(initialLocation);
+      }, function() {
+        handleNoGeolocation(browserSupportFlag);
+      }); 
+    }
+    //browser does not support geolocation
+    else{
+      browserSupportFlag = false;
+      handleNoGeolocation(browserSupportFlag);
+    }
+
+    function handleNoGeolocation(errorFlag){
+      if (errorFlag == true) {
+        alert("Geolocation service failed.");
+        initialLocation = mitCampus;
+      }else{
+        alert("Browser does not support Geolocation.");
+        initialLocation = mitCampus;
+      }
+      map.setCenter(initialLocation);
+    }
+}}
 
 if (Meteor.isServer) {
   Meteor.startup(function () {
