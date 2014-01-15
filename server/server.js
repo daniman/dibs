dea1d403433004297bb1e0ce05ae2050f82b5550
Danielle Man <daniman0808@gmail.com>
2014-01-15 12:24:56 -0500

    updated accordion colors

--- server/server.js
+++ server/server.js
@@ -1,9 +1,9 @@
 Meteor.startup(function () {
 	//setup for Parse API
-	Parse.initialize("KxXRF1qcFjHqA2AKnyPvg5Ys2VzMWR2ViAKNtX8V", "sXds7BqaSnQTpfszRNgQ0UO5h17fkGdIZwWgNi0A");
-	var TestObject = Parse.Object.extend("TestObject");
-	var testObject = new TestObject();
-	testObject.save({foo: "bar"}).then(function(object) {
-	  alert("yay! it worked");
-	});
+	// Parse.initialize("KxXRF1qcFjHqA2AKnyPvg5Ys2VzMWR2ViAKNtX8V", "sXds7BqaSnQTpfszRNgQ0UO5h17fkGdIZwWgNi0A");
+	// var TestObject = Parse.Object.extend("TestObject");
+	// var testObject = new TestObject();
+	// testObject.save({foo: "bar"}).then(function(object) {
+	//   alert("yay! it worked");
+	// });
 });
