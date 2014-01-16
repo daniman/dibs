a245c0c4bb6eedc0f47719df334b86935899f8b6
Andrew Mikofalvy <mikofalv@mit.edu>
2014-01-16 00:00:51 -0500

    added parse support

--- server/server.js
+++ server/server.js
@@ -1,9 +1,9 @@
 Meteor.startup(function () {
-	//setup for Parse API
-	Parse.initialize("KxXRF1qcFjHqA2AKnyPvg5Ys2VzMWR2ViAKNtX8V", "sXds7BqaSnQTpfszRNgQ0UO5h17fkGdIZwWgNi0A");
-	var TestObject = Parse.Object.extend("TestObject");
-	var testObject = new TestObject();
-	testObject.save({foo: "bar"}).then(function(object) {
-	  alert("yay! it worked");
+	result = Meteor.http.get("https://api.parse.com/1/classes/SuperDildo", {
+	   headers: {
+	      "X-Parse-Application-Id": "KxXRF1qcFjHqA2AKnyPvg5Ys2VzMWR2ViAKNtX8V",
+	      "X-Parse-REST-API-Key": "ES02u7gPWXSmOshD1Lwo2yhxjj4j0fxbcqdTUSBE"
+	   }
 	});
+	console.log(result);
 });
