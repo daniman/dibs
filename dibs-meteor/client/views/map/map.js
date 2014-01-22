5236bdcf5faa4e3e13c59c2854090f105549a52e
Danielle Man <daniman0808@gmail.com>
2014-01-21 19:50:46 -0500

    map only renders once now (instead of 30+ times it was before)

--- dibs-meteor/client/views/map/map.js
+++ dibs-meteor/client/views/map/map.js
@@ -1,6 +1,7 @@
 // This function is run when the page renders on client's browser
-Template.page.rendered = function() {// Geolocation Vars for setting up map and default position.
-	var initialLocation;
+Template.map.rendered = function() {// Geolocation Vars for setting up map and default position.
+	console.log("rendered");
+  var initialLocation;
 	var defaultLocation = new google.maps.LatLng (42.357,-71.09); // the lat/long of a default location. Set to central campus at MIT
 	var browserSupportFlag = new Boolean(); // A flag to keep track if the clients browser supports geolocation
 
@@ -25,73 +26,25 @@ Template.page.rendered = function() {// Geolocation Vars for setting up map and 
         styles: myStyles
     };
 
-	gmaps.initialize(mapOptions);
-  console.log("here");
+    gmaps.initialize(mapOptions);
 
-//----------------------------------------------------------------------------------------------------------//
+    var cursorMarker = Posts.find();
+    cursorMarker.observeChanges({
+      added: function(id, fields){
+        var post = {
+          lat: fields.latitude,
+          lng: fields.longitude,
+          title: fields.title,
+          content:fields.content,
+          author: fields.author,
+          postTime: fields.postTime
+        };
+        
+        gmaps.addMarker(post);
 
-  // google.maps.event.addListener(map, 'click', function(event) {
-
-  //   var infoWindow = new google.maps.InfoWindow({
-  //     content: '<form id="newItemForm">Post a new thing on dibs!' + 
-  //               '<br><input id="newItemTitle" type="text" name="title" placeholder="Title">' + 
-  //               '<br><input type="text" id="newItemDescription" name="description" placeholder="Description">' + 
-  //               '<br><input id="submitNewItem" type="submit" value="Post!" />' + 
-  //               '</form>'
-  //   });
-
-  //   var marker = new google.maps.Marker({
-  //     position: event.latLng,
-  //     map: map,
-  //     icon: 'http://maps.google.com/mapfiles/ms/icons/green-dot.png',
-  //     title: "New Item!"
-  //   })
-
-  //   google.maps.event.addListener(marker, 'click', function() {
-  //     infoWindow.open(map,marker);
-  //   });
-
-  //   google.maps.event.addListener(infoWindow, 'domready', function() {
-  //     $("#newItemForm").submit(function(e) {
-  //       var title = $("#newItemTitle").val();
-  //       var description = $("#newItemDescription").val();
-  //       console.log("meow");
-  //       console.log($("#newItemTitle").val());
-  //       console.log($("#newItemDescription").val());
-  //       Items.insert({
-  //         title: title,
-  //         description: description
-  //       });
-  //       infoWindow.setContent("<p>" + title + "</p>" + "<p>" + description + "</p>");
-  //       $("#postListContainer").prepend("<div class='post'><div class='title'>" + title + "</div><div class='description'>" + description + "</div></div>")
-  //       e.preventDefault();
-  //     });
-  //   });
-
-  //   infoWindow.open(map, marker);
-
-  // });\\
-
-var cursorMarker = Posts.find();
-cursorMarker.observeChanges({
-	added: function(id, fields){
-		var post = {
-			lat: fields.latitude,
-			lng: fields.longitude,
-			title: fields.title,
-			content:fields.content,
-			author: fields.author,
-			postTime: fields.postTime
-		};
-		
-		gmaps.addMarker(post);
-    console.log("gmaps");
-
-	},
-	removed: function(id) {
-		//marker.setMap(null);
-	}
-});
-
+      },
+      removed: function(id) {
+        //marker.setMap(null);
+      }
+    });
 }
-
