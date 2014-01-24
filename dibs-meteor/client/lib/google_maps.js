e28ade363daf6f2113525ccd363271cd1d5b0bd1
Danielle Man <daniman0808@gmail.com>
2014-01-24 11:58:33 -0500

    added color scale

--- dibs-meteor/client/lib/google_maps.js
+++ dibs-meteor/client/lib/google_maps.js
@@ -318,5 +318,5 @@ gmaps = {
 formatDate = function(utcDate) {
 	var tmpDate = new Date(utcDate);
 	tmpDate = tmpDate + "";
-	return tmpDate.slice(0, tmpDate.length-15);
+	return tmpDate.slice(0, tmpDate.length-18);
 }
