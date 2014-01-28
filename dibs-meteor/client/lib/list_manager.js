e90174eafb995df3a465a8501516f1baf1dd242c
Danielle Man <daniman0808@gmail.com>
2014-01-28 01:11:49 -0500

    fixed addClass problem with clicking on markers and postlist

--- dibs-meteor/client/lib/list_manager.js
+++ dibs-meteor/client/lib/list_manager.js
@@ -1,14 +1,15 @@
 listmanager = {
 	clearListFormatting: function() {
+		console.log("clear");
 		$('.post').removeClass('post-active');
 		// console.log('cleared post list formatting');
 	},
 
 	setListFocus: function(_id) {
-		console.log("setListFocus");
 		listmanager.clearListFormatting();
-		console.log(_id);
-		$("#category_"+_id+".post").addClass('post-active');
-		$('#postListContainer').animate({scrollTop: $("#category_"+_id).offset().top});
+		setTimeout(function() {
+            $('#category_'+_id).addClass('post-active');
+        }, 100);
+		$('#category_'+_id).ScrollTo();
 	}
 }
